efe071b3cc5eaa7663e58231915327bf06c55833
Shariar Islam <shariarn85@gmail.com>
2023-11-05 18:13:17 +0600

    get food, request food, cancel food api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
@@ -29,6 +30,12 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const foodCollection = client.db('HungerHelpNetDB').collection('foodCollection');
+const requestCollection = client.db('HungerHelpNetDB').collection('requestCollection');
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -46,9 +53,77 @@ async function run() {
 run().catch(console.dir);
 
 
+// -----------:: GET Operation ::----------------
+
 app.get('/', (req, res) => {
     res.send('HungerHelpNet server is running')
 })
+
+
+app.get('/foods', async (req, res) => {
+  try {
+
+    const result = await foodCollection.find().toArray()
+
+    
+    res.send(result)
+    
+  } catch (error) {
+    console.log('getting error from get /food', error);
+  }
+})
+
+
+// -----------:: POST Operation ::----------------
+
+app.post('/request-food', async (req, res) => {
+  try {
+    const foodRequest = req.body;
+    const result = await requestCollection.insertOne(foodRequest)
+
+    
+    res.send(result)
+    
+  } catch (error) {
+    console.log('getting error from post /request-food', error);
+  }
+})
+
+
+app.post('/access-token', async (req, res) => {
+  try {
+
+    
+  } catch (error) {
+    console.log('getting error from post /access-token'. error);
+  }
+})
+
+// -----------:: PUT Operation ::----------------
+
+
+
+// -----------:: PATCH Operation ::----------------
+
+
+
+// -----------:: DELETE Operation ::----------------
+
+app.delete('/cancel-food/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await requestCollection.deleteOne(query)
+    res.send(result)
+
+  } catch (error) {
+    console.log('getting error form delete /request-food/:id', error);
+  }
+
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`HungerHelpNet server is running on the port: ${port}`);
